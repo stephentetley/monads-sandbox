30de8a671caeff823cad0bf5a81a40046365741f
stephentetley <stephen.tetley@gmail.com>
2021-01-23 12:31:40 +0000

    More additions.

--- hs/Test01.hs
+++ hs/Test01.hs
@@ -3,7 +3,19 @@
 module Test where
 
 import Control.Applicative
+import Control.Monad
 
 -- | One or none.
 optionalA :: Alternative f => f a -> f (Maybe a)
 optionalA v = (Just <$> v) <|> pure Nothing
+
+
+maybe_mzero :: Maybe String
+maybe_mzero = mzero
+
+list_mzero :: [Int]
+list_mzero = mzero
+
+list_mplus01 :: [Int]
+list_mplus01 = [1] `mplus` [2,3,4]
+
